5916413c479be1e149aab37981e87be62b28a5ab
Konieczny Nataniel <nataniel.k08@gmail.com>
2023-09-25 17:54:52 +0200

    zad2

--- stefanczyk/lekcja2/zad2/script.js
+++ stefanczyk/lekcja2/zad2/script.js
@@ -2,5 +2,21 @@ $(document).ready(function () {
   // $(window).on("mousedown", function (e) {
   //     console.log("x: " + e.clientX + ", y: " + e.clientY)
   // })
-  $(".middleDiv").on("mousedown", function () {});
+  $(".middleDiv").on("mousedown", function (e) {
+    console.log("x: " + e.clientX + " y: " + e.clientY);
+  });
+
+  $(".middleDiv").on("mousedown", () => {
+    $(window).on("mousemove", (e) => {
+      let offsetLeftDiv = $(".leftDiv").offset().left;
+      let rightWidth =
+        $(window).width() - $(".leftDiv").width() - $(".middleDiv").width();
+      $(".leftDiv").css("width", e.clientX - offsetLeftDiv);
+      $(".rightDiv").css("width", rightWidth);
+      $(".middleDiv").css("left", e.clientX - offsetLeftDiv);
+    });
+    $(".middleDiv").on("mouseup", () => {
+      $(window).off("mousemove");
+    });
+  });
 });
