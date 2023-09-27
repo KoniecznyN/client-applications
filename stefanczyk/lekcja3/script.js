b8c32cff0a93ed1edb6600caba76292f80c5dd51
Konieczny Nataniel <nataniel.k08@gmail.com>
2023-09-27 20:38:09 +0200

    lekcja3

---
+++ stefanczyk/lekcja3/script.js
@@ -0,0 +1,63 @@
+$(document).ready(function () {
+  const count = 5;
+  let tab = [];
+  let playerOne = $("<div>").addClass("playerOne");
+  let playerTwo = $("<div>").addClass("playerTwo");
+  $(".main").append(playerOne).append(playerTwo);
+  for (let i = 0; i < count; ++i) {
+    for (let j = 0; j < count; ++j) {
+      let box = $("<div>").addClass("box");
+      $(".main").append(box);
+      $(box)
+        .css("left", i * 50)
+        .css("top", j * 50)
+        .html(`${i}-${j}`)
+        .on("click", function () {
+          $(box).css("background-color", "yellow");
+          tab.push({ x: $(this).css("left"), y: $(this).css("top") });
+          console.log(tab);
+        });
+    }
+  }
+
+  $("#start").on("click", function () {
+    if (tab.length >= 3) {
+      playerOneX = tab[0].x;
+      playerOneY = tab[0].y;
+      $(playerOne).css("left", playerOneX).css("top", playerOneY);
+      playerTwoX = tab[tab.length - 1].x;
+      playerTwoY = tab[tab.length - 1].y;
+      $(playerTwo).css("left", playerTwoX).css("top", playerTwoY);
+    } else {
+      console.log("błędne dane");
+    }
+  });
+
+  let helpyy = true;
+  let helpyOne = 1;
+  let helpyTwo = 2;
+  $("#move").on("click", function () {
+    if (helpyy) {
+      playerOneX = tab[helpyOne].x;
+      playerOneY = tab[helpyOne].y;
+      $(playerOne).css("left", playerOneX).css("top", playerOneY);
+      ++helpyOne;
+    } else {
+      playerTwoX = tab[tab.length - helpyTwo].x;
+      playerTwoY = tab[tab.length - helpyTwo].y;
+      $(playerTwo).css("left", playerTwoX).css("top", playerTwoY);
+      ++helpyTwo;
+    }
+    helpyy = !helpyy;
+  });
+
+  $("#reset").on("click", () => {
+    $(".box").css("background-color", "white");
+    $(playerOne).css("left", "-50px").css("top", "0");
+    $(playerTwo).css("left", "-50px").css("top", "50px");
+    tab = [];
+    helpyy = true;
+    helpyOne = 1;
+    helpyTwo = 2;
+  });
+});
