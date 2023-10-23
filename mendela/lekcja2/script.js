2c4df4e8410d42888f768003649102a24dcba0f7
Konieczny Nataniel <nataniel.k08@gmail.com>
2023-10-23 23:42:23 +0200

    dna

--- mendela/lekcja2/script.js
+++ mendela/lekcja2/script.js
@@ -2,15 +2,18 @@ function DNA(dna) {
   let dnaA = "";
   let dnaArray = [];
   let dnaArrayB = [];
-
   let charsDnaArrayB = { A: "T", T: "A", C: "G", G: "C" };
+
+  dna = dna.replaceAll(/[^a-z]/gi, "");
 
   for (let i = 0; i < dna.length; i += 3) {
     dnaA = dna.slice(i, i + 3).toUpperCase();
     dnaArray.push(dnaA);
   }
 
-  console.log(dnaArray);
+  dnaArray.forEach((element) => {
+    dnaArrayB.push(element.replace(/[TACG]/g, (n) => charsDnaArrayB[n]));
+  });
 
   let dnaStats = [];
   for (let i = 0; i < dnaArray.length; ++i) {
@@ -20,18 +23,26 @@ function DNA(dna) {
         counter += 1;
       }
       if (j == dnaArray.length - 1) {
-        let dnaStatsElement = `${dnaArray[i]} - ${counter}`;
+        let dnaStatsElement = { kodon: dnaArray[i], count: counter };
         dnaStats.push(dnaStatsElement);
       }
     }
   }
 
-  console.log(dnaStats);
-  console.log(dnaStats[2]);
+  let dnaStatsA = [];
+  for (let i = 0; i < dnaStats.length; ++i) {
+    if (dnaStats[i].count != -1) {
+      dnaStatsA.push(JSON.parse(JSON.stringify(dnaStats[i])));
+    }
+    let kodon = dnaStats[i].kodon;
+    for (let j = i; j < dnaStats.length; ++j) {
+      if (kodon == dnaStats[j].kodon) {
+        dnaStats[j].count = -1;
+      }
+    }
+  }
 
-  dnaArray.forEach((element) => {
-    dnaArrayB.push(element.replace(/[TACG]/g, (m) => charsDnaArrayB[m]));
-  });
+  dnaStatsA.sort((a, b) => (a.count < b.count ? 1 : -1));
 
   dnaArray = dnaArray.join();
   dnaArrayB = dnaArrayB.join();
@@ -44,10 +55,21 @@ function DNA(dna) {
   dnaArray = dnaArray.replaceAll(",", " ");
   dnaArrayB = dnaArrayB.replaceAll(",", " ");
 
-  let result = `${dnaArray} <br><br> ${dnaArrayB}`;
+  document.write(`${dnaArray} <br><br> ${dnaArrayB}`);
 
-  return result;
+  let color = "";
+  for (let i = 0; i < dnaStatsA.length; ++i) {
+    if (i % 5 == 0) {
+      const R = Math.round(Math.random() * 255);
+      const G = Math.round(Math.random() * 255);
+      const B = Math.round(Math.random() * 255);
+      color = `${R}, ${G}, ${B}`;
+    }
+    document.write(
+      `<p style="background-color: rgb(${color}); padding: 5px; margin: 0;">${dnaStatsA[i].kodon} - ${dnaStatsA[i].count}</p>`
+    );
+  }
 }
 
 let dna = prompt();
-document.write(DNA(dna));
+DNA(dna);
