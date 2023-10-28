e6e4f202fd915f3fc41b31f1652badc953cd80ae
Konieczny Nataniel <nataniel.k08@gmail.com>
2023-10-28 14:06:11 +0200

    lekcja 3 mendela

---
+++ mendela/lekcja3/zad1/script.js
@@ -0,0 +1,11 @@
+function dupa() {
+  let image = document.getElementById("deathnote");
+
+  const element = document.getElementById("dupa");
+
+  setInterval(function () {
+    element.innerHTML += "Hello";
+  }, 1000);
+}
+
+dupa();
