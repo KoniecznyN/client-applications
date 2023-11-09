92d423c729eeea21079fc506558b27a18944286f
Konieczny Nataniel <nataniel.k08@gmail.com>
2023-11-09 20:44:09 +0100

    u

--- mendela/lekcja3/zad4/script.js
+++ mendela/lekcja3/zad4/script.js
@@ -1,23 +1,28 @@
 const main = document.getElementById("main");
 const box = document.getElementById("box");
 
-const radius = 200;
-const centerX = container.offsetWidth / 2;
-const centerY = container.offsetHeight / 2;
-const speed = 1;
+let centerX = main.clientWidth / 2;
+let centerY = main.clientHeight / 2;
 
+const radius = 100;
+const angularSpeed = 5;
 let angle = 0;
+let dupa = 1;
 
-function animate() {
+function moveInCircle() {
+  let boxPosition = { top: box.style.top, left: box.style.left };
+
   const x = centerX + radius * Math.cos(angle);
   const y = centerY + radius * Math.sin(angle);
 
-  box.style.left = x - box.offsetWidth / 2 + `px`;
-  box.style.top = y - box.offsetHeight / 2 + `px`;
+  box.style.left = x - box.clientWidth / 2 + "px";
+  box.style.top = y - box.clientWidth / 2 + "px";
 
-  angle += speed * 0.01;
+  angle += angularSpeed * 0.01;
+  // console.log(box.style.left);
+  // console.log(box.style.top);
 
-  requestAnimationFrame(animate);
+  requestAnimationFrame(moveInCircle);
 }
 
-animate();
+moveInCircle();
