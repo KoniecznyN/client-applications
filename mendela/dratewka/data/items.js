9eec14f28179e4f83336bb43d776763185d3f317
Konieczny Nataniel <nataniel.k08@gmail.com>
2024-03-03 17:32:27 +0100

    dratewka manipulacja przedmiotami

--- mendela/dratewka/data/items.js
+++ mendela/dratewka/data/items.js
@@ -1,25 +1,52 @@
 class Item {
-    name;
-    flag;
-    x;
-    y;
+  inputName;
+  flag;
+  name;
 
-    constructor(name, flag, x, y) {
-        this.name=name
-        this.flag=flag
-        this.x=x
-        this.y=y
-    }
+  constructor(inputName, flag, name) {
+    this.inputName = inputName;
+    this.flag = flag;
+    this.name = name;
   }
+}
 
-  export const items = [
-    new Item("", 1, 3, 1),
-    new Item("", 1, 5, 1),
-    new Item("", 1, 7, 1),
-    new Item("", 1, 3, 2),
-    new Item("", 1, 7, 2),
-    new Item("", 1, 2, 3),
-    new Item("", 1, 4, 4),
-    new Item("", 1, 5, 5),
-    new Item("", 1, 4, 6)
-  ]
+export const items = [
+  new Item("a KEY", 1, "KEY"),
+  new Item("an AXE", 1, "AXE"),
+  new Item("STICKS", 1, "STICKS"),
+  new Item("sheeplegs", 0, "sheeplegs"),
+  new Item("MUSHROOMS", 1, "MUSHROOMS"),
+  new Item("MONEY", 1, "MONEY"),
+  new Item("a BARREL", 1, "BARREL"),
+  new Item("a sheeptrunk", 0, "sheeptrunk"),
+  new Item("BERRIES", 1, "BERRIES"),
+  new Item("WOOL", 1, "WOOL"),
+  new Item("a sheepskin", 0, "sheepskin"),
+  new Item("a BAG", 1, "BAG"),
+  new Item("a RAG", 1, "RAG"),
+  new Item("a sheephead", 0, "sheephead"),
+  new Item("a SPADE", 1, "SPADE"),
+  new Item("SULPHUR", 1, "SULPHUR"),
+  new Item("a solid poison", 0, "solid poison"),
+  new Item("a BUCKET", 1, "BUCKET"),
+  new Item("TAR", 1, "TAR"),
+  new Item("a liquid poison", 0, "liquid poison"),
+  new Item("a dead dragon", 0, "dead dragon"),
+  new Item("a STONE", 1, "STONE"),
+  new Item("a FISH", 1, "FISH"),
+  new Item("a KNIFE", 1, "KNIFE"),
+  new Item("a DRAGONSKIN", 1, "DRAGONSKIN"),
+  new Item("a dragonskin SHOES", 1, "SHOES"),
+  new Item("a PRIZE", 1, "PRIZE"),
+  new Item("a SHEEP", 1, "SHEEP"),
+];
+
+export function displayItem(array, name) {
+  let item;
+  array.forEach((element) => {
+    if (element.name === name) {
+      item = element;
+    }
+  });
+  return item;
+}
--- mendela/dratewka/script.js
+++ mendela/dratewka/script.js
@@ -1,5 +1,7 @@
 import { locations } from "./data/locations.js";
-import { items } from "./data/items.js";
+import { displayItem } from "./data/items.js";
+
+console.log(locations);
 
 const game = {
   x: 6,
@@ -7,12 +9,14 @@ const game = {
   init() {
     this.displayLocation();
   },
-  itemCarried: [{ name: "nothing", flag: 1 }],
+  itemCarried: { inputName: "nothing" },
   inputActions: {
     N: "NORTH",
     S: "SOUTH",
     W: "WEST",
     E: "EAST",
+    T: "TAKE",
+    D: "DROP",
     V: "VOCABULARY",
     G: "GOSSIP",
   },
@@ -21,6 +25,14 @@ const game = {
     let what = directions
       .map((element) => this.inputActions[element])
       .join(", ");
+
+    let items = "";
+    for (let i = 0; i < locations[this.y][this.x].item.length; i++) {
+      items += locations[this.y][this.x].item[i].inputName;
+      if (i < locations[this.y][this.x].item.length - 1) {
+        items += ", ";
+      }
+    }
 
     const template = `
         <h2>${locations[this.y][this.x].description}</h2>
@@ -38,8 +50,8 @@ const game = {
         </div>
         <div id="textarea">
           <p>You can go ${what}</p>
-          <p>You see nothing</p>
-          <p>You are carrying ${this.itemCarried[0].name}</p>
+          <p>You see ${items}</p>
+          <p>You are carrying ${this.itemCarried.inputName}</p>
           <label id="eventText" for="action">What now?</label>
           <input type="text" name="action" id="action" autofocus>
         </div>
@@ -82,26 +94,89 @@ const game = {
     let direction = {};
     let action = "";
     let input = document.getElementById("action").value.toUpperCase();
+    input = input.split(" ");
     let possibleDirections = locations[this.y][this.x].directions;
     let canGo = false;
 
     Object.entries(this.inputActions).forEach((element) => {
-      if (input == element[0] || input == element[1]) {
-        input = element[1];
+      if (input[0] == element[0] || input[0] == element[1]) {
+        input[0] = element[1];
       }
     });
 
-    if (input == "NORTH") {
+    //wybranie akcji
+    if (input[0] == "NORTH") {
       direction = { shortcut: "N", vector: { x: 0, y: -1 } };
-    } else if (input == "SOUTH") {
+    } else if (input[0] == "SOUTH") {
       direction = { shortcut: "S", vector: { x: 0, y: 1 } };
-    } else if (input == "EAST") {
+    } else if (input[0] == "EAST") {
       direction = { shortcut: "E", vector: { x: 1, y: 0 } };
-    } else if (input == "WEST") {
+    } else if (input[0] == "WEST") {
       direction = { shortcut: "W", vector: { x: -1, y: 0 } };
-    } else if (input == "VOCABULARY") {
+    }
+
+    //take
+    else if (input[0] == "TAKE") {
+      let itemExist = false;
+      let itemIndex = 0;
+      for (let i = 0; i < locations[this.y][this.x].item.length; i++) {
+        if (locations[this.y][this.x].item[i].name == input[input.length - 1]) {
+          itemExist = true;
+          itemIndex = i;
+          break;
+        }
+      }
+      if (itemExist) {
+        if (this.itemCarried.inputName == "nothing") {
+          document.getElementById("eventText").innerText = `You are taking ${
+            locations[this.y][this.x].item[itemIndex].inputName
+          }`;
+          this.itemCarried = locations[this.y][this.x].item[itemIndex];
+          locations[this.y][this.x].item.splice(itemIndex, 1);
+          if (locations[this.y][this.x].item.length == 0) {
+            locations[this.y][this.x].item.push({ inputName: "nothing" });
+          }
+        } else {
+          document.getElementById(
+            "eventText"
+          ).innerText = `You are carrying something`;
+        }
+      } else {
+        document.getElementById(
+          "eventText"
+        ).innerText = `There isn't anything like that here`;
+      }
+    }
+
+    //drop
+    else if (input[0] == "DROP") {
+      if (this.itemCarried.inputName != "nothing") {
+        if (input[input.length - 1] == this.itemCarried.name) {
+          document.getElementById(
+            "eventText"
+          ).innerText = `You are about to drop ${this.itemCarried.inputName}`;
+          if (locations[this.y][this.x].item[0].inputName != "nothing") {
+            locations[this.y][this.x].item.push(this.itemCarried);
+          } else {
+            locations[this.y][this.x].item[0] = this.itemCarried;
+          }
+          this.itemCarried = { inputName: "nothing" };
+        } else {
+          document.getElementById(
+            "eventText"
+          ).innerText = `You are not carrying it`;
+        }
+      } else {
+        document.getElementById(
+          "eventText"
+        ).innerText = `You are not carrying anything`;
+      }
+    }
+
+    //reszta akcji
+    else if (input[0] == "VOCABULARY") {
       action = "V";
-    } else if (input == "GOSSIP") {
+    } else if (input[0] == "GOSSIP") {
       action = "G";
     } else {
       this.displayLocation();
@@ -109,6 +184,28 @@ const game = {
       return;
     }
 
+    //poruszanie sie
+    if (direction.shortcut != undefined) {
+      possibleDirections.forEach((element) => {
+        if (element == direction.shortcut) {
+          canGo = true;
+        }
+      });
+
+      if (canGo) {
+        this.x += direction.vector.x;
+        this.y += direction.vector.y;
+        document.getElementById(
+          "eventText"
+        ).innerText = `You are going ${input}`;
+      } else {
+        document.getElementById(
+          "eventText"
+        ).innerText = `You can't go that way`;
+      }
+    }
+
+    //gossip i vocabulary
     if (action.length != 0) {
       let template = "";
       if (action == "V") {
@@ -156,19 +253,7 @@ const game = {
       return;
     }
 
-    possibleDirections.forEach((element) => {
-      if (element == direction.shortcut) {
-        canGo = true;
-      }
-    });
-
-    if (canGo) {
-      this.x += direction.vector.x;
-      this.y += direction.vector.y;
-      document.getElementById("eventText").innerText = `You are going ${input}`;
-    } else {
-      document.getElementById("eventText").innerText = `You can't go that way`;
-    }
+    //end
     document.getElementById("action").style.display = "none";
     setTimeout(() => {
       this.displayLocation();
