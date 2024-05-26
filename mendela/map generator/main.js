1c7ac12f12ecd6034c61af18e99e839cd22aac4d
KoniecznyN <nataniel.k08@gmail.com>
2024-05-26 16:00:17 +0200

    mapgenerator cz.2

---
+++ mendela/map generator/main.js
@@ -0,0 +1,53 @@
+var firstGrid = document.getElementById("firstGrid");
+var secondGrid = document.getElementById("secondGrid");
+var Tile = /** @class */ (function () {
+    function Tile(x, y, background, content) {
+        this.x = x;
+        this.y = y;
+        this.background = background;
+        var div = document.createElement("div");
+        div.className = background;
+        var text = document.createTextNode(content);
+        div.append(text);
+        this.content = div;
+    }
+    return Tile;
+}());
+var Grid = /** @class */ (function () {
+    function Grid(rows, cols, name) {
+        this.content = [];
+        for (var i = 0; i < rows; i++) {
+            this.content[i] = [];
+            for (var j = 0; j < cols; j++) {
+                this.content[i][j] = new Tile(j, i, "tile", "");
+                // `/*x: ${j}, y:${i}*/`
+            }
+        }
+        this.name = name;
+    }
+    Grid.prototype.converter = function () {
+        var _this = this;
+        this.content.forEach(function (element) {
+            var row = document.createElement("div");
+            row.className = "row";
+            element.forEach(function (element) {
+                row.append(element.content);
+            });
+            _this.name.append(row);
+        });
+    };
+    return Grid;
+}());
+var Game = /** @class */ (function () {
+    function Game() {
+    }
+    Game.prototype.startGame = function () {
+        var tablica1 = new Grid(32, 16, firstGrid);
+        var tablica2 = new Grid(48, 48, secondGrid);
+        tablica1.converter();
+        tablica2.converter();
+    };
+    return Game;
+}());
+var game = new Game();
+game.startGame();
---
+++ mendela/map generator/main.ts
@@ -0,0 +1,63 @@
+const firstGrid = document.getElementById("firstGrid") as HTMLBodyElement
+const secondGrid = document.getElementById("secondGrid") as HTMLBodyElement
+
+class Tile{
+    x:number
+    y:number
+    background:string
+    content:HTMLDivElement
+
+    constructor(x:number, y:number, background:string, content:string){
+        this.x = x
+        this.y = y
+        this.background = background
+        
+        const div = document.createElement("div")
+        div.className = background
+        let text = document.createTextNode(content)
+        div.append(text)
+
+        this.content = div
+    }
+}
+
+class Grid{
+    rows:number
+    cols:number
+    name:HTMLBodyElement
+    content:Tile[][] = []
+    constructor(rows:number, cols:number, name:HTMLBodyElement){
+        for (let i = 0; i < rows; i++) {
+            this.content[i]=[]
+           for (let j = 0; j < cols; j++) {
+            this.content[i][j] = new Tile(j,i,"tile","")
+            // `/*x: ${j}, y:${i}*/`
+           } 
+        }
+
+        this.name = name
+    }
+
+    converter(){
+        this.content.forEach(element => {
+            const row = document.createElement("div")
+            row.className = "row"
+            element.forEach(element => {
+                row.append(element.content)
+            });
+            this.name.append(row)
+        });
+    }
+}
+
+class Game {
+    startGame(){
+        let tablica1 = new Grid(32,16,firstGrid)
+        let tablica2 = new Grid(48,48,secondGrid)
+        tablica1.converter()
+        tablica2.converter()
+    }
+}
+
+const game = new Game();
+game.startGame()
